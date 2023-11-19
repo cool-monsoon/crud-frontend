cc392c6c39d729dddff2cf5b87396a3f9878edcf
cool-monsoon <102756118+cool-monsoon@users.noreply.github.com>
2023-11-19 14:43:15 +0300

    Next

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,19 @@
 import {Link} from 'react-router-dom';
 import moment from 'moment';
-import PropTypes from "prop-types";
 
-export default function Post(props) {
-const { post } = props;
+export default function Post ({post}) {
   return(
-    <>
-      {post.map((element)=>
-        <Link to={`/posts/${element.id}`} key={element.id} >
-          <div className="post">
-            <p className='time'>{moment(element.created).format('DD/MM/YYYY HH:mm')}</p>
-            <p className="post_content">{element.content}</p> 
-          </div>
-        </Link>
-      )}
-    </>
-  )
-
-  Post.propTypes = {
-    post: PropTypes.array
-   };
+  <>
+    {post?.map((element)=>
+      <Link to={`/posts/${element.id}`} key={element.id} >
+        <div className="post">
+          <p className='time'>{moment(element.created).format('DD/MM/YYYY HH:mm')}</p>
+          <p className="post_content">{element.content}</p> 
+        </div>
+      </Link>
+    )}
+  </>
+)
 
 }
 
