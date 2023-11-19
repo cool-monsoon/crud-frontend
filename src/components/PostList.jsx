81f080cc65e3f939bb620cfe9cc96eac29fc7d57
cool-monsoon <102756118+cool-monsoon@users.noreply.github.com>
2023-11-19 14:12:28 +0300

    PostList update

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,30 +2,26 @@ import { useEffect, useState } from "react"
 import Post from "./Post"
 import NewPostButton from "./NewPostButton"
 import { Routes, Route } from "react-router-dom"
-
+import axios from "axios";
 
 export default function PostList() {
 
-  const [post,setPost] = useState()
-  const getPost = () =>{
-    fetch('http://localhost:7070/posts')
-      .then((response)=>response.json())
-      .then((data)=>setPost(data))
-      .catch(error=>{
-        console.error(error)
-     });
+  const [post, setPost] = useState([])
+  const getPosts = () => {
+    const response = axios.get("http://localhost:7070/posts");
+    const data = response.data;
+    setPost(data);
+
   }
 
-  useEffect(getPost,[])
+  useEffect(getPosts,[])
 
   return (
     <>
       <NewPostButton/>
-        <div className="posts-container">
-          <Routes>
-            <Route path="/posts" element={<Post post = {post} />}/>
-          </Routes>
-        </div>
+      <div className="posts-container">
+        <Post post = {post} />
+      </div>
     </>
   )
 }
