9a8280c04f315423866281fd73d70b6a09122a7a
cool-monsoon <102756118+cool-monsoon@users.noreply.github.com>
2023-11-25 11:24:10 +0300

    finally it works

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import PostList from './components/PostList'
 import NewPost from './components/NewPost'
 import PostView from './components/PostView'
-import EditPost from './components/EditPost'
+import PostEditing from './components/PostEditing'
 
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route path='posts/' element={<PostList/>}/>
         <Route path='posts/new' element={<NewPost/>}/>
         <Route path='posts/:id' element={<PostView/>}/>
-        <Route path='posts/:id/edit' element={<EditPost/>}/>
+        <Route path='posts/:id/edit' element={<PostEditing/>}/>
       </Routes>
     </div>
   );
--- src/components/PostEditing.jsx
+++ src/components/PostEditing.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom"
 import axios from 'axios';
 
@@ -10,7 +10,17 @@ export default function PostEditing() {
   const {id} = useParams();
   const [text, setText] = useState({content: ''});
 
-  console.log('initial text', text);
+  console.log('initial', text);
+
+  useEffect(()=>{
+    fetch(`http://localhost:7070/posts/${id}`)
+    .then(response =>response.json())
+    .then((data)=>setText(data))
+    .catch(error=>{
+        console.error(error)
+    });
+},[id])
+
 
   const handleChange = event => {
     const newText = event.target.value;
@@ -22,7 +32,16 @@ export default function PostEditing() {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (id > 0){
-      axios.post(`http://localhost:7070/posts`, {id: id, content: text})
+      axios.put(`http://localhost:7070/posts/${id}`, {id: id, content: text})
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        throw new Error('No response');
+    })
+      .catch(error => {
+        console.error('Ошибка:', error);
+        });
       setText({content: text});
     }
   }
@@ -32,7 +51,7 @@ export default function PostEditing() {
   }
 
   return (
-    <>
+    <div className="card">
       <form className="edit-card" onSubmit={handleSubmit}>
         <h3>Редактировать публикацию</h3>
         <textarea rows="6" cols="50" value={text.content} onChange={handleChange}/>
@@ -41,7 +60,7 @@ export default function PostEditing() {
          </div>
       </form>
       <button className='close' onClick={(event)=>{handleClick(event)} }>&#10006;</button>
-    </>
+    </div>
   );
 }
 
